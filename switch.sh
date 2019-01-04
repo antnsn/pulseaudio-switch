0a33a2298217704815eb66c47785dc4e85d9489b
anbugecko <antonsen.marius@gmail.com>
2019-01-05 00:32:06 +0100

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -1,16 +1,37 @@
 #!/bin/sh
 
-DEVICE='3'
-ACTIVE_SINK=$(pacmd list-sinks | grep 'active port: <analog-output-lineout>' | awk '{ print $3 }')
+SPEAKERS="alsa_output.pci-0000_0d_00.3.iec958-stereo"
+HEADSET="alsa_output.pci-0000_0d_00.3.analog-stereo"
+ACTIVE_SINK=$(pactl list short | grep RUNNING | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,')
 
-if [ "$ACTIVE_SINK" = "<analog-output-lineout>" ]; then
+if [ "$ACTIVE_SINK" = "4" ]; then
+	(
+	echo set-default-sink $SPEAKERS
 	echo "[*] Switching to Speakers."
-	pacmd set-card-profile $DEVICE output:iec958-stereo
+	
+	pacmd list-sink-inputs |
+		grep -E '^\s*index:' |
+		grep -oE '[0-9]+' |
+		while read input
+	do
+		echo move-sink-input $input $SPEAKERS
+	done
+	) | pacmd > /dev/null
+	
 else
+	(
+	echo set-default-sink $HEADSET
 	echo "[*] Switching to headphones."
-	pacmd set-card-profile $DEVICE output:analog-stereo
+	
+	pacmd list-sink-inputs |
+		grep -E '^\s*index:' |
+		grep -oE '[0-9]+' |
+		while read input
+	do
+		echo move-sink-input $input $HEADSET
+	done
+	) | pacmd > /dev/null
 fi
 
 exit 0
-
 
