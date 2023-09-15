52dab7f840fee3381283e26d7c75c8a8ce54dcc6
Marius Antonsen <antonsen.marius@gmail.com>
2023-09-16 01:45:32 +0200

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -1,39 +1,29 @@
 #!/bin/sh
 
+# Define the sink names for speakers and headphones
 SPEAKERS="alsa_output.pci-0000_0d_00.3.iec958-stereo"
 HEADSET="alsa_output.pci-0000_0d_00.3.analog-stereo"
-ACTIVE_SINK=$(pactl list short | grep -E '^(6.)|(alsa_output.pci-0000_0d_00.3*)' | grep -w RUNNING | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,')
 
-if [ "$ACTIVE_SINK" = "7" ]; then
-	(
-	echo set-default-sink $SPEAKERS
-	notify-send 'Switching to Speakers.' --icon=dialog-information
-		
-	pacmd list-sink-inputs |
-		grep -E '^\s*index:' |
-		grep -oE '[0-9]+' |
-		while read input
-	do
-		echo move-sink-input $input $SPEAKERS
-	done
-	) | pacmd > /dev/null
-	
-elif [ "$ACTIVE_SINK" = "6" ]; then
-	(
-	echo set-default-sink $HEADSET
-	notify-send 'Switching to Headphones.' --icon=dialog-information
-	
-	pacmd list-sink-inputs |
-		grep -E '^\s*index:' |
-		grep -oE '[0-9]+' |
-		while read input
-	do
-		echo move-sink-input $input $HEADSET
-	done
-	) | pacmd > /dev/null
-fi
+# Get the index of the active audio sink
+ACTIVE_SINK=$(pactl list short sinks | grep -E '^(6|7).*$' | awk '{print $1}')
+
+# Function to switch the audio output and move sink inputs
+switch_audio_output() {
+  local new_sink="$1"
+  notify-send "Switching to $new_sink" --icon=dialog-information
+
+  pacmd set-default-sink "$new_sink"
+  pacmd list-sink-inputs | grep -E '^\s*index:' | awk '{print $2}' | while read input
+  do
+    pacmd move-sink-input "$input" "$new_sink"
+  done
+}
+
+# Perform the audio output switch based on the active sink
+case "$ACTIVE_SINK" in
+  7) switch_audio_output "$SPEAKERS";;
+  6) switch_audio_output "$HEADSET";;
+  *) echo "Unknown active sink: $ACTIVE_SINK";;
+esac
 
 exit 0
-
-
-
